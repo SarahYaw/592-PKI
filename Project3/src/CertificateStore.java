2b1c694228156faaa17807f5a13de8ab729084a5
Brian Urbancic <burbanci@emich.edu>
2021-11-23 07:08:05 -0500

    Added contains() to CertificateStore, TestCertificates file

--- Project3/src/CertificateStore.java
+++ Project3/src/CertificateStore.java
@@ -9,23 +9,54 @@ we will add the ability for users to request/create certificates and other funct
 */
 import java.util.ArrayList;
 
+import working.Certificate;
+
+
 public class CertificateStore {
+
+//	private ArrayList<Certificate> certs = new ArrayList<Certificate>();
+	private ArrayList<Certificate> certs;
+
+	public CertificateStore() {
+		this.certs = new ArrayList<Certificate>();
+	}
 	
-	
-	public static void main(String[] args) {
-		ArrayList<Certificate> certs = new ArrayList<Certificate>();
-		Certificate c1 = new Certificate();
-		Certificate c2 = new Certificate("Bob Smith", "DH", "These Params", "19d09baa34e");
-		c2.setEndDate("2021-10-13");
-		certs.add(c1);
-		certs.add(c2);
-		
-		
-		for (Certificate c : certs) {
-			System.out.println(c);
-			
-		}
+	public void createStore() {
+//		All certificates but Brain are valid with an ultra super top secret private key: "privateKey"
+		this.certs.add(new Certificate("2021-12-31", "Brian", "DH", "These Params", "privateKey"));
+		this.certs.add(new Certificate("2021-01-01", "Brain", "DH", "These Params", "privateKey"));
+		this.certs.add(new Certificate("2021-12-31", "Sarah", "DH", "These Params", "privateKey"));
+		this.certs.add(new Certificate("2021-12-31", "Payton", "DH", "These Params", "privateKey"));
+		this.certs.add(new Certificate("2021-12-31", "Fatema", "DH", "These Params", "privateKey"));
+		this.certs.add(new Certificate("2021-12-31", "Yu", "DH", "These Params", "privateKey"));
 		
 	}
+	
+//	public getCerts
+
+	public ArrayList<Certificate> getCerts() {
+		return certs;
+	}
+
+	public void setCerts(ArrayList<Certificate> certs) {
+		this.certs = certs;
+	}
+	
+	/*
+	A method to check if the CertificateStore object contains a certificate by checking username and key pair.
+	then checking if the certificate is expired.
+	*/
+	public boolean contains(String user, String userKey) {
+		
+		for (Certificate c : this.certs) {
+			if (c.userAndKeyMatch(user, userKey)) {
+				return(c.dateIsValid());
+			}		
+		}
+		System.out.println("Error: No match for user and key found.");
+		
+		return false;
+	}
+
 
 }
